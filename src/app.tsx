757fa6fbbe925c325d5eb116dda85745e4897e5c
nesh <nesh@ubuntu-24.localhost>
2026-04-17 23:51:39 +0000

    Merge template into platform

---
+++ src/app.tsx
@@ -0,0 +1,29 @@
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import { Suspense } from "solid-js";
+import { MetaProvider } from "@solidjs/meta";
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
+import { AuthProvider } from "~/lib/guards/auth";
+import { StoreProvider } from "~/lib/store-context";
+import "./app.css";
+import Home from "./home"
+// Shared QueryClient instance - required so all components share the same cache.
+// Without this, invalidating/refetching queries from one component won't affect others.
+const queryClient = new QueryClient()
+
+export default function App() {
+
+  return (
+    <MetaProvider>
+      <QueryClientProvider client={queryClient}>
+        <StoreProvider id="st_55mvdfu3yfoq">
+          <Router
+            root={Home}
+          >
+            <FileRoutes />
+          </Router>
+        </StoreProvider>
+      </QueryClientProvider>
+    </MetaProvider>
+  );
+}
